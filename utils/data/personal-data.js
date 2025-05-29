bc02e61216bbbb79f7e0325322a34a583cfb4ec7
seiixin <sidneygarcinan@gmail.com>
2025-05-29 17:04:37 +0800

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Sidney Pagdanganan",
   profile: '/profile.png',
   designation: "Web Developer",
-  description: "My name is Sidney Pagdanganan. My name is Sidney Pagdanganan. I am a passionate and dedicated web developer with a strong focus on Laravel and PHP. I enjoy solving problems, building user-friendly web applications, and constantly learning new technologies. I’m comfortable working across the full stack, but my core strength lies in back-end development using Laravel. I love creating clean, secure, and efficient systems that make life easier for users. I’m always open to opportunities that match my skills and allow me to grow as a developer.",
+  description: "My name is Sidney Pagdanganan. I am a passionate and dedicated web developer with a strong focus on Laravel and PHP. I enjoy solving problems, building user-friendly web applications, and constantly learning new technologies. I’m comfortable working across the full stack, but my core strength lies in back-end development using Laravel. I love creating clean, secure, and efficient systems that make life easier for users. I’m always open to opportunities that match my skills and allow me to grow as a developer.",
   email: 'sidneypagdanganan@gmail.com',
   phone: '09946260502',
   address: 'Molino IV, Bacoor, Cavite - 4102',
